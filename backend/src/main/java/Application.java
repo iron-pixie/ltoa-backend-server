e5915ea6424047cb572b5f676d08cb1d127a1d17
Stephen Harb <stephen.harb@ironpixietechnologies.com>
2018-06-07 21:18:43 -0500

    add maven framework

--- backend/src/main/java/Application.java
+++ backend/src/main/java/Application.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.web.servlet.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
