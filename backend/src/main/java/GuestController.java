0f842e96a744f1ac43dc0162eca6f94e2243745c
Stephen Harb <stephen.harb@ironpixietechnolgoies.com>
2018-09-03 16:27:42 -0500

    Develop

--- backend/src/main/java/GuestController.java
+++ backend/src/main/java/GuestController.java
@@ -24,30 +24,64 @@ public class GuestController {
     String residentAdress;
     String carModel;
     String carMake;
+    String guests;
 
     @CrossOrigin(origins = "*")
-    @RequestMapping(value = "/guest/{residentAdress}")
-    public HashMap<String, String> guestsRequest(@PathVariable("residentAdress") String ra)
+    @RequestMapping(value = "/guest/all")
+    public ArrayList<HashMap<String, String>> worksRequestAll()
     {  try {
+        ArrayList<HashMap<String, String>> guest_map_array = new ArrayList();
         HashMap<String, String> guest_map = new HashMap<String, String>();
+        guests = "";
         Class.forName("com.mysql.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://aadnxib9b7f6cj.cebbknh24dty.us-west-2.rds.amazonaws.com:3306/guest", "test", "testtest");
+        Connection con = DriverManager.getConnection("jdbc:mysql://aadnxib9b7f6cj.cebbknh24dty.us-west-2.rds.amazonaws.com:3306/guests", "test", "testtest");
         Statement stmt=con.createStatement();
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         GuestController obj = (GuestController) context.getBean("GuestBean");
-        String queryString = "select * from Guests where residentAdress = " + ra;
-        ResultSet rs = stmt.executeQuery(queryString);
-        rs.next();
-        guest_map.put("guestName", rs.getString(1));
-        guest_map.put("residentAdress", rs.getString(2));
-        guest_map.put("carModel", rs.getString(3));
-        guest_map.put("carMake", rs.getString(4));
-        return guest_map;
+        ResultSet rs = stmt.executeQuery("select * from Guests");
+        while(rs.next())
+        {
+            HashMap<String, String> guest_remap = new HashMap<String, String>();
+            guest_remap.put("guestName", rs.getString(1));
+            guest_remap.put("residentAddress", rs.getString(2));
+            guest_remap.put("carModel", rs.getString(3));
+            guest_remap.put("carMake", rs.getString(4));
+            guest_map_array.add(guest_remap);
+        }
+        return guest_map_array;
     }
     catch(Exception exception)
     {
         guest_map.put("Error", exception.toString());
-        return guest_map;
+        guest_map_array.add(guest_map);
+        return guest_map_array;
+    }
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/guest/add", method = RequestMethod.POST)
+    @ResponseBody
+    public String LoginAdd(@RequestBody HashMap<String, String> guestList)
+    {  try {
+
+        this.setGuestName(guestList.get("guestName"));
+        this.setResidentAdress(guestList.get("residentAdress"));
+        this.setCarModel(guestList.get("carMake"));
+        this.setCarMake(guestList.get("carModel"));
+
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection cons = DriverManager.getConnection("jdbc:mysql://aadnxib9b7f6cj.cebbknh24dty.us-west-2.rds.amazonaws.com:3306/guests", "test", "testtest");
+        Statement stmts = cons.createStatement();
+        String queryString = "insert into Guests(guestName, residentAddress, carModel, carMake)  values ('" + this.getGuestName() + "', '" + this.getResidentAdress() + "', '" + this.getCarModel() + "', '" + this.getCarMake() + "')";
+        stmts.executeUpdate(queryString);
+        EmailServices emailServices = new EmailServices();
+        String emailMessage = "A new guest has been added with name: " + this.getGuestName() + "They are associated with address: " + this.getResidentAdress();
+        emailServices.sendMailAccess(("New Guest, Name: " + this.getGuestName()), emailMessage);
+        return "Successful addition of row";
+    }
+    catch(Exception exception)
+    {
+        return exception.toString();
     }
     }
 
