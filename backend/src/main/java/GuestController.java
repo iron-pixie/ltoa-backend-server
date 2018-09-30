32c19802891c51d7119e0cb56f44d063f54ea10b
Stephen Harb <stephen.harb@ironpixietechnolgoies.com>
2018-09-30 16:07:46 -0500

    Develop

--- backend/src/main/java/GuestController.java
+++ backend/src/main/java/GuestController.java
@@ -25,15 +25,20 @@ public class GuestController {
     private String guestName;
     private String residentName;
     private String allowedStartTime;
-    private String allowedEndTime
+    private String allowedEndTime;
     private String residentAddress;
     private String carModel;
     private String carMake;
     private String guests;
+    private String mostRecentEntry;
+    private String entryTime;
+    private String reason;
+    private DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    private Date date = new Date();
 
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/guest/all")
-    public ArrayList<HashMap<String, String>> worksRequestAll()
+    public ArrayList<HashMap<String, String>> guestRequestAll()
     {  try {
         ArrayList<HashMap<String, String>> guest_map_array = new ArrayList();
         HashMap<String, String> guest_map = new HashMap<String, String>();
@@ -48,8 +53,14 @@ public class GuestController {
             guest_remap.put("residentAddress", rs.getString(2));
             guest_remap.put("carModel", rs.getString(3));
             guest_remap.put("carMake", rs.getString(4));
+            guest_remap.put("residentName", rs.getString(5));
+            guest_remap.put("allowedStartTime", rs.getString(6));
+            guest_remap.put("allowedEndTime", rs.getString(7));
+            guest_remap.put("mostRecentEntry", rs.getString(8));
+            guest_remap.put("reason", rs.getString(9));
             guest_map_array.add(guest_remap);
         }
+
         return guest_map_array;
     }
     catch(Exception exception)
@@ -63,23 +74,101 @@ public class GuestController {
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/guest/add", method = RequestMethod.POST)
     @ResponseBody
-    public String LoginAdd(@RequestBody HashMap<String, String> guestList)
+    public String guestAdd(@RequestBody HashMap<String, String> guestList)
     {  try {
 
         this.setGuestName(guestList.get("guestName"));
         this.setResidentAddress(guestList.get("residentAddress"));
         this.setCarModel(guestList.get("carMake"));
         this.setCarMake(guestList.get("carModel"));
+        this.setResidentName("residentName");
+        this.setAllowedStartTime("allowedStartTime");
+        this.setAllowedEndTime("allowedEndTime");
+        this.setMostRecentEntry("mostRecentEntry");
+        this.setReason("reason");
 
         Class.forName("com.mysql.jdbc.Driver");
         Connection cons = DriverManager.getConnection("jdbc:mysql://aadnxib9b7f6cj.cebbknh24dty.us-west-2.rds.amazonaws.com:3306/guests", "test", "testtest");
         Statement stmts = cons.createStatement();
-        String queryString = "insert into Guests(guestName, residentAddress, carModel, carMake)  values ('" + this.getGuestName() + "', '" + this.getResidentAddress() + "', '" + this.getCarModel() + "', '" + this.getCarMake() + "')";
+        String queryString = "insert into Guests(guestName, residentAddress, carModel, carMake, residentName, allowedStartTime, allowedEndTime, reason)  values ('" + this.getGuestName() + "', '" + this.getResidentAddress() + "', '" + this.getCarModel() + "', '" + this.getCarMake() +  "', '" + this.getResidentName() + "', '" + this.getAllowedStartTime() +  "', '" + this.getAllowedEndTime() +  "', '" + this.getReason() + "')";
         stmts.executeUpdate(queryString);
         EmailServices emailServices = new EmailServices();
         String emailMessage = "A new guest has been added with name: " + this.getGuestName() + "They are associated with address: " + this.getResidentAddress();
         emailServices.sendMailAccess(("New Guest, Name: " + this.getGuestName()), emailMessage);
         return "Successful addition of row";
+    }
+    catch(Exception exception)
+    {
+        return exception.toString();
+    }
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @ResponseBody
+    public String guestRegister(@RequestBody HashMap<String, String> registerMap)
+    {  try {
+
+        this.setGuestName(registerMap.get("guestName"));
+        this.setResidentAddress(registerMap.get("residentAddress"));
+        this.setResidentName(registerMap.get("residentName"));
+        this.setEntryTime(dateFormat.format(date));
+
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection cons = DriverManager.getConnection("jdbc:mysql://aadnxib9b7f6cj.cebbknh24dty.us-west-2.rds.amazonaws.com:3306/guests", "test", "testtest");
+        Statement stmts = cons.createStatement();
+        String queryString = "insert into Registry(guestName, residentAddress, residentName, entryTime)  values ('" + this.getGuestName() + "', '" + this.getResidentAddress() + "', '" + this.getResidentName() + "', '" + this.getEntryTime() + "')";
+        stmts.executeUpdate(queryString);
+        EmailServices emailServices = new EmailServices();
+        String emailMessage = "A new guest has passed the gate and registered with the guards with name: " + this.getGuestName() + "They are associated with address: " + this.getResidentAddress() + ". And the time of entry was: "+this.getEntryTime();
+        emailServices.sendMailAccess(("Guest, Name: " + this.getGuestName()), emailMessage);
+        return "Successful addition of row";
+    }
+    catch(Exception exception)
+    {
+        return exception.toString();
+    }
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/register/all")
+    public ArrayList<HashMap<String, String>> registerRequestAll()
+    {  try {
+        ArrayList<HashMap<String, String>> guest_map_array = new ArrayList();
+        HashMap<String, String> guest_map = new HashMap<String, String>();
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://aadnxib9b7f6cj.cebbknh24dty.us-west-2.rds.amazonaws.com:3306/guests", "test", "testtest");
+        Statement stmt=con.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from Registry");
+        while(rs.next())
+        {
+            HashMap<String, String> guest_remap = new HashMap<String, String>();
+            guest_remap.put("guestName", rs.getString(1));
+            guest_remap.put("residentAddress", rs.getString(2));
+            guest_remap.put("residentName", rs.getString(2));
+            guest_remap.put("entryTime", rs.getString(4));
+            guest_map_array.add(guest_remap);
+        }
+        return guest_map_array;
+    }
+    catch(Exception exception)
+    {
+        guest_map.put("Error: ", exception.toString());
+        guest_map_array.add(guest_map);
+        return guest_map_array;
+    }
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/guest/delete/{guestName}", method = RequestMethod.DELETE)
+    public String GuestsRequestDelete(@PathVariable("guestName") String guestName)
+    {  try {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://aadnxib9b7f6cj.cebbknh24dty.us-west-2.rds.amazonaws.com:3306/guests", "test", "testtest");
+        Statement stmt=con.createStatement();
+        String queryString = "delete from Guests where guestName = " + guestName;
+        stmt.executeUpdate(queryString);
+        return "Successful Deletion of Row";
     }
     catch(Exception exception)
     {
@@ -106,6 +195,30 @@ public class GuestController {
     {
         return this.carMake;
     }
+
+    private String getResidentName() {return this.residentName;}
+
+    private String getAllowedStartTime() {return this.allowedStartTime;}
+
+    private String getAllowedEndTime() {return this.allowedEndTime;}
+
+    private String getMostRecentEntry() {return this.mostRecentEntry;}
+
+    private String getEntryTime() {return this.entryTime;}
+
+    private String getReason() {return this.reason;}
+
+    private void setReason(String Reason) {this.reason = Reason;}
+
+    private void setResidentName(String ResidentName) {this.residentName = ResidentName;}
+
+    private void setAllowedStartTime(String AllowedStartTime) {this.allowedStartTime = AllowedStartTime;}
+
+    private void setAllowedEndTime(String AllowedEndTime) {this.allowedEndTime = AllowedEndTime;}
+
+    private void setMostRecentEntry(String MostRecentEntry) {this.mostRecentEntry = MostRecentEntry;}
+
+    private void setEntryTime(String EntryTime) {this.entryTime = EntryTime;}
 
     private void setGuestName(String GuestName)
     {
